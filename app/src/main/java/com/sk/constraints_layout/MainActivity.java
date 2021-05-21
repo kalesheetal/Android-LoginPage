39ba906bb135261176ff36bbb447db58af28f1e2
kalesheetal <sheetalkale20201@gmail.com>
2021-05-21 11:48:21 +0530

    Login Page using constraint layout

---
+++ app/src/main/java/com/sk/constraints_layout/MainActivity.java
@@ -0,0 +1,27 @@
+package com.sk.constraints_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"submitted",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
